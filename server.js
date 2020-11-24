465ed4cb317fc87e587c8be9311bf7c0fe600540
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-11-24 11:14:49 +0200

    API request

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const api = require('./Exam/routes/api')
+const api = require('./server/routes/api')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use('/', api)
 
-const port = 3000 
+const port = 8080
 app.listen(port, function(){
     console.log(`server is running on port ${port}`)
 })
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,19 @@
 const express = require("express");
-// const urllib = require("urllib");
+const urllib = require("urllib");
 const router = express.Router();
 
-router.get("/:param", function (req, res) {
-    
-  });
+router.get("/sanity", function(req,res){
+  res.send("OK")
+})
+
+router.get("/recipes/:ingredient", function (req, res) {
+  const ingredient = req.params.ingredient
+    urllib.request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function(err, response){
+      const recipes = JSON.parse(response)
+      console.log(recipes.results)
+      res.send(recipes.results)
+    })
+})
 
 
 module.exports = router;
